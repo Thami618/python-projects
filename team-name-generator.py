f47e72aeee2a8464e30239748075dcb6c2187c7d
Thamsanqa Ngcobo <thamsanqa.ngcobo@altron.com>
2022-03-01 23:08:43 +0200

    Team Name Gnerator

---
+++ team-name-generator.py
@@ -0,0 +1,8 @@
+#Greeting
+print('Welcome to the Team name Generator')
+
+#prompt user city they grew up in
+city = input('Please enter your prefered city name\n')
+catchy_name = input('Please enter a suitable catchy name\n')
+
+print('Your brand name is ' + city + ' ' + catchy_name)
