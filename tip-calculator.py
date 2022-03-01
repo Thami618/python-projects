cad1dfea84da0cd83eec23b1d544ae56439968cb
Thamsanqa Ngcobo <thamsanqa.ngcobo@altron.com>
2022-03-02 00:58:45 +0200

    The Tip Calculator

---
+++ tip-calculator.py
@@ -0,0 +1,11 @@
+print("Welcome to the tip calculate")
+
+total_bill = float(input("What was your total bill amount"))
+tip_perc = int(input("how much tip are you gonna give 10, 15 or 20"))
+people = int(input("How many people are you goimg to split the bill between?"))
+
+tip_as_perc = tip_perc / 100
+total_tip_amt = total_bill * tip_as_perc
+total_bill_amt = total_bill + total_bill
+amt_per_person = round(total_bill / people, 2) 
+print(f"each person should pay {amt_per_person}")
